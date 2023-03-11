f3c6eb98b919e9aa9ef748d68f96b5bc1d05f716
Jetmir <Jetmirdemiri2002@gmail.com>
2023-03-11 22:14:16 +0100

    Ndryshim ne vue.config.js

--- my-project/vue.config.js
+++ my-project/vue.config.js
@@ -1,30 +1,7 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
+ const { defineConfig } = require('@vue/cli-service')
+ module.exports = defineConfig({
+   transpileDependencies: true
+ })
 
 
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = {
-  transpileDependencies: true,
-
-  devServer: {
-    before: app => {
-      app.use(
-        '/api',
-        createProxyMiddleware({
-          target: 'https://api.football-data.org/v4',
-          changeOrigin: true,
-          headers: {
-            'X-Auth-Token': 'feea40ee4ac1414a831b124e97e6d650',
-          },
-          pathRewrite: {
-            '^/api': '',
-          },
-        })
-      );
-    },
-  },
-};
